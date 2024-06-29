fd64c4041e767c5e6278faf508f67d7fcce53eaf
Derek Miranda <derek.miranda@hearst.com>
2024-06-28 23:59:54 -0400

    display button info

--- src/components/Clicker.tsx
+++ src/components/Clicker.tsx
@@ -10,18 +10,33 @@ import { saveGameData } from "../storage";
 import { ClickerContext } from "../reducers/context";
 import useDispatchers from "../hooks/useDispatchers";
 import { useContext } from "react";
+import {
+  EffectTypes,
+  UpdateResourcesEffect,
+  UpdateResourcesRateEffect,
+} from "../types";
 
 interface ButtonProps extends ButtonInterface {
   clickButton: (buttonId: string) => void;
 }
 
-function Button({ id, displayName, clickButton, cooldown }: ButtonProps) {
+function Button({
+  id,
+  displayName,
+  clickButton,
+  enabled,
+  cooldown,
+  effects,
+  oneTime,
+  purchased,
+  cost,
+}: ButtonProps) {
   const handleClick = () => {
     clickButton(id);
   };
   const opacity = cooldown?.onCooldown
     ? cooldown.elapsedCooldownSeconds / cooldown.cooldownSeconds
-    : 1;
+    : undefined;
 
   return (
     <>
@@ -33,27 +48,60 @@ function Button({ id, displayName, clickButton, cooldown }: ButtonProps) {
           opacity,
           cursor: cooldown?.onCooldown ? "wait" : "default",
         }}
-        disabled={cooldown?.onCooldown}
+        disabled={!enabled || cooldown?.onCooldown}
         onClick={handleClick}
       >
         {displayName}
+        {cost ? (
+          <span>
+            Cost:{" "}
+            {Object.entries(cost)
+              .map(([key, val]) => `${val} ${key}`)
+              .join(", ")}
+          </span>
+        ) : null}
+        {effects.length ? (
+          <span>
+            Effect:{" "}
+            {effects.map((effect) => {
+              if (effect.type === EffectTypes.UPDATE_RESOURCES) {
+                const { resourcesDiff } = effect as UpdateResourcesEffect;
+                return Object.entries(resourcesDiff)
+                  .map(
+                    ([resourceKey, resourceVal]) =>
+                      `+${resourceVal} ${resourceKey}`
+                  )
+                  .join(", ");
+              } else if (effect.type === EffectTypes.UPDATE_RESOURCES_RATE) {
+                const { resourcesRateDiff } =
+                  effect as UpdateResourcesRateEffect;
+                return Object.entries(resourcesRateDiff)
+                  .map(
+                    ([resourceKey, resourceVal]) =>
+                      `+ ${resourceVal} ${resourceKey}/sec`
+                  )
+                  .join(", ");
+              }
+            })}
+          </span>
+        ) : null}
+        {oneTime ? (
+          <span>{purchased ? "Purchased" : "One Time Purchase"}</span>
+        ) : null}
       </button>
     </>
   );
 }
 
+function formatNum(num: number) {
+  return num.toFixed(2);
+}
+
 function Clicker() {
   const { state } = useContext(ClickerContext);
   const { clickButton, tickClock, clearGameData } = useDispatchers();
-  const { resources, buttons } = state;
-  const {
-    energy,
-    maxEnergy,
-    co2Saved,
-    knowledge,
-    globalPpm,
-    globalPpmPerMonth,
-  } = resources;
+  const { resources, resourceGrowthRates, buttons } = state;
+  const { energy, maxEnergy, co2Saved, knowledge, globalPpm } = resources;
 
   useTicker((timeDelta) => {
     tickClock(timeDelta);
@@ -64,14 +112,19 @@ function Clicker() {
       <h1>Carbon Clicker</h1>
       <Modal />
       <div>
-        <p className="ppm-display">Global CO2: {globalPpm} PPM</p>
-        <p>CO2 Growth Rate: {globalPpmPerMonth} PPM/month</p>
+        <p className="ppm-display">Global CO2: {formatNum(globalPpm)} PPM</p>
+        {resourceGrowthRates.globalPpm ? (
+          <p>
+            CO2 Growth Rate: {formatNum(resourceGrowthRates.globalPpm * 30)}{" "}
+            PPM/month
+          </p>
+        ) : null}
         <p>--</p>
         <p>
           Mood: {energy}/{maxEnergy}
         </p>
         <p>Knowledge: {knowledge}</p>
-        <p>CO2 Saved: {co2Saved} kg</p>
+        <p>CO2 Saved: {formatNum(co2Saved)} kg</p>
         <div className="buttons-container">
           {buttons.order
             .filter((buttonKey) => buttons.map[buttonKey].unlocked)
