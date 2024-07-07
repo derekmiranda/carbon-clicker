7d9c76249778873b4c6d314d62c5ef343c9d4933
Derek Miranda <derek.miranda@hearst.com>
2024-07-07 19:31:52 -0400

    scroll logs back up when update

--- src/components/Logs.tsx
+++ src/components/Logs.tsx
@@ -1,12 +1,22 @@
-import { useContext } from "react";
+import { useContext, useEffect, useRef } from "react";
 import "./Logs.css";
 import { ClickerContext } from "../reducers/context";
 
 export default function Logs() {
   const { state } = useContext(ClickerContext);
+  const boxRef = useRef<HTMLDivElement>(null);
+
   const { logs } = state;
+
+  // scroll back to top when logs change
+  useEffect(() => {
+    if (boxRef.current) {
+      boxRef.current.scrollTo(0, 0);
+    }
+  }, [logs]);
+
   return (
-    <div className="logs-box">
+    <div className="logs-box" ref={boxRef}>
       {logs
         .map((log, i) => (
           <p className="log" key={i}>
