a92b2409138cbf4eff8ed56bf0b0e67607e316bf
Derek Miranda <derek.miranda@hearst.com>
2024-07-09 23:42:09 -0400

    create phase two test data

--- src/data/clicker.ts
+++ src/data/clicker.ts
@@ -1,7 +1,13 @@
-import { MAX_MOOD, STARTING_PPM, STARTING_PPM_RATE } from "../constants";
+import {
+  MAX_MOOD,
+  SELF_EDUCATE_THRESHOLDS,
+  STARTING_PPM,
+  STARTING_PPM_RATE,
+} from "../constants";
+import { ButtonInterface } from "../reducers/buttonReducer";
 import { ClickerInterface } from "../reducers/clickerReducer";
-import { GamePhase, ModalView } from "../types";
-import { buttons } from "./buttons";
+import { ButtonKey, GamePhase, ModalView } from "../types";
+import { buttons, PHASE_TWO_SELF_EDUCATE_EFFECTS } from "./buttons";
 
 export const clicker: ClickerInterface = {
   modalQueue: [{ view: ModalView.INTRO }],
@@ -21,12 +27,50 @@ export const clicker: ClickerInterface = {
     peoplePower: 0,
     trust: 0,
     collectiveDollars: 0,
+  }, // diff per second
+  resourceGrowthRates: {
+    globalPpm: STARTING_PPM_RATE,
+  },
+  buttons,
+  elapsedTime: 0,
+  endgameSelfEducateTimesPressed: 0,
+};
+
+// test data for phase 2
+export const phaseTwoClicker: ClickerInterface = {
+  modalQueue: [{ view: ModalView.END_PHASE_ONE }],
+  logs: [],
+  ppmEventIndex: 3,
+  storySeen: {},
+  phase: GamePhase.TWO,
+  resources: {
+    mood: 200,
+    maxMood: MAX_MOOD,
+    dollars: 1000,
+    co2Saved: 1000,
+    knowledge: 50,
+    globalPpm: 427,
+
+    // phase 2
+    peoplePower: 0,
+    trust: 0,
+    collectiveDollars: 0,
   },
   // diff per second
   resourceGrowthRates: {
     globalPpm: STARTING_PPM_RATE,
   },
-  buttons,
+  buttons: {
+    ...clicker.buttons,
+    map: {
+      ...clicker.buttons.map,
+      [ButtonKey.selfEducate]: {
+        ...(clicker.buttons.map[ButtonKey.selfEducate] as ButtonInterface),
+        timesPressed: SELF_EDUCATE_THRESHOLDS.PHASE_TWO + 1,
+        effects: PHASE_TWO_SELF_EDUCATE_EFFECTS,
+      },
+    },
+  },
   elapsedTime: 0,
   endgameSelfEducateTimesPressed: 0,
 };
