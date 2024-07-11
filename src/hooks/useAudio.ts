126b837bf42cf14273c28f8c98e1ea6b024ed554
Derek Miranda <derek.miranda@hearst.com>
2024-07-11 09:17:24 -0400

    lower default volume

--- src/hooks/useAudio.ts
+++ src/hooks/useAudio.ts
@@ -1,7 +1,11 @@
 import { Howl } from "howler";
 import { useEffect } from "react";
 
+export const DEFAULT_VOLUME = 0.4;
+
 export default function useAudio() {
+  Howler.volume(DEFAULT_VOLUME);
+
   useEffect(() => {
     const sound = new Howl({
       src: [`${import.meta.env.BASE_URL}clicker-sprites.mp3`],
