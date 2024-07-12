3667118f6abf37e7f549e453ae7e5417b3b92bcc
Derek Miranda <derek.miranda@hearst.com>
2024-07-12 09:45:10 -0400

    upgrades > one-time actions

--- src/components/Upgrades.tsx
+++ src/components/Upgrades.tsx
@@ -7,7 +7,7 @@ export default function Upgrades() {
   return upgradeButtons.length ? (
     <div className="buttons-container buttons-container--upgrades">
       <div className="buttons-box">
-        <h2>Upgrades</h2>
+        <h2>One-Time Actions</h2>
         <ButtonsList buttonOrder={upgradeButtons} />
       </div>
     </div>
